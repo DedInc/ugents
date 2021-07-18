bafaf9b6cd5e9a80372462c615670c7b36dd7c70
DedInc <41906303+DedInc@users.noreply.github.com>
2021-07-18 17:55:08 +0600

    v1.0.1

--- ugents/ugents.py
+++ ugents/ugents.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
-from random import randint as rnt
+from random import randint as rnt, choice
 from requests import get
 from json import load
 from os.path import dirname, abspath, join
 
-def choice(arr):
-	return arr[rnt(0, len(arr)-1)]
-
-def getAgent():
+def getAgent(browser='all', os='Windows'):
 	with open(join(dirname(abspath(__file__)), 'agents.json')) as file:
 		agents = load(file)
 	browsers = ['chrome', 'firefox', 'opera']
 	agent = ''
 	i = rnt(0, 3)
-	if i == 0:
+	if browser == 'all' and i == 0 or browser == 'chrome':
 		agent = 'Mozilla/' + choice(agents['mozilla'])
 	elif i == 1 or i == 2:
 		agent = 'Mozilla/5.0'
 	else:
 		agent = 'Opera/' + choice(agents['operav'])
 	agent = agent + ' ' + choice(agents['os'])
-	browser = choice(browsers)
+	if browser == 'all':
+		browser = choice(browsers)
 	if agent.__contains__('Opera'):
 		browser = 'opera'
 		agent = agent + ' Presto/' + choice(agents['presto'])
